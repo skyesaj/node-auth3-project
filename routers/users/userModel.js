027fc32597bfcaf008b95262857da5cc7f051639
skye <skyesajous@gmail.com>
2020-02-26 16:34:11 -0700

    mvp, first streatch

--- routers/users/userModel.js
+++ routers/users/userModel.js
@@ -3,7 +3,11 @@ const db = require("../../data/dbConfig");
 function get() {
   return db("user");
 }
+function getByDep(dep) {
+  return db("user").where({ department: dep });
+}
 
 module.exports = {
-  get
+  get,
+  getByDep
 };
--- routers/users/userRouter.js
+++ routers/users/userRouter.js
@@ -3,14 +3,34 @@ const router = require("express").Router();
 const user = require("./userModel");
 
 router.get("/", (req, res) => {
-  user
-    .get()
-    .then(user => {
-      res.status(200).json(user);
-    })
-    .catch(error => {
-      res.status(500).json({ error: "no" });
-    });
+  //   user
+  //     .get()
+  //     .then(user => {
+  //       res.status(200).json(user);
+  //     })
+  //     .catch(error => {
+  //       res.status(500).json({ error: "no" });
+  //     });
+  // });
+  if (req.user.user_department === "admin") {
+    user
+      .get()
+      .then(users => {
+        res.status(200).json(users);
+      })
+      .catch(err => {
+        res.status(500).json({ errorMessage: err.message });
+      });
+  } else {
+    user
+      .getByDep(req.user.user_department)
+      .then(users => {
+        res.status(200).json(users);
+      })
+      .catch(err => {
+        res.status(500).json({ error: err.message });
+      });
+  }
 });
 
 module.exports = router;
